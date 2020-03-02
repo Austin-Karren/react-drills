0ebe204813e67b8de0ab942e0913b7530b7e356c
Austin-Karren <austin.t.karren@gmail.com>
2020-03-02 16:56:47 -0700

    more practice

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,45 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 import Todo from './Todo'
 
 class App extends Component {
-constructor() {
-  super();
-  this.state = {
-    list: [],
-    input: ''
+  constructor(props){
+    super(props);
+    this.state = { list: [], input: ''}
   }
-  this.handleAddTask = this.handleAddTask.bind(this);
-}
 
-handleInput(val) { this.setState({input: val}) };
-handleAddTask() {
-  this.setState({
-    list: [...this.state.list, this.state.input],
-    input: ''
-  })
-}
-
-  render() {
-    let list = this.state.list.map((element, index) => {
-      return <Todo key={index} task={element} />;
+  handleInput = (value) => this.setState({input: value});
+  handleAddTask = () => {
+    this.setState({
+      list: [...this.state.list, this.state.input],
+      input: ''
     });
-
-    return (
-       <div className='App'>
-         <h1> My to-do list: </h1>
-         <div>
-          <input value={this.state.input}
-          placeholder='Enter new task'
-          onChange={e => this.handleInput(e.target.value)}/>
-          <button onClick={this.handleAddTask}> Add </button>
-         </div>
-         <br/>
-         {list}
-       </div>
-    );
   }
+  handleEnterKey = (event) => {
+    if (event.key === 'Enter'){
+      console.log('Enter key hit')
+      this.handleAddTask(event.target.value);
+    }
+  }
+
+  render(){
+    let displayList = this.state.list.map((element, index) => {
+      return <Todo key={index} task={element}></Todo>
+    })
+    return(
+      <div className="App">
+        <h1>Todo List</h1>
+        <div>
+        <input 
+        type="text" 
+        placeholder="Enter new task :)" 
+        value={this.state.input} 
+        onChange={e => this.handleInput(e.target.value)}
+        onKeyPress={e => this.handleEnterKey(e)}
+        />
+        <button onClick={this.handleAddTask}>Add</button>
+        </div>
+
+        <br/>
+        {displayList}
+      </div>
+    )
+  }
+
 }
 
 export default App;
